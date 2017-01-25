6ac9b71700d74d8e0b8790905438e2c356937582
Steve Witman <stevewitman@gmail.com>
2017-01-24 23:58:09 -0700

    Add basic-mathematical-operations

--- kyu8/challenges.js
+++ kyu8/challenges.js
@@ -41,3 +41,34 @@ function reverse(string){
 }
 
 reverse = s => s.split(' ').reverse().join(' ')
+
+/ #####################################################################
+// https://www.codewars.com/kata/basic-mathematical-operations
+// TAGS: if/else case
+
+function basicOp(operation, value1, value2) {
+  if (operation === '+') {
+    return value1 + value2
+  } else if (operation === '-') {
+    return value1 - value2
+  } else if (operation === '*') {
+    return value1 * value2
+  } else if (operation === '/') {
+    return value1 / value2
+  }
+}
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case '+':
+      return value1 + value2;
+    case '-':
+      return value1 - value2;
+    case '*':
+      return value1 * value2;
+    case '/':
+      return value1 / value2;
+    default:
+      return 0;
+  }
+}
